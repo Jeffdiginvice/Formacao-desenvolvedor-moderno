0165561ccf551b93cfecc82ddd10f7d0e15692be
Jeffdiginvice <jeffersondiginvice7@gmail.com>
2023-02-06 13:25:58 -0300

    atualizar

---
+++ aulas/exercicios/operadores/operadores.js
@@ -0,0 +1,12 @@
+ //Operadores Aritméticos
+ console.log("Operadores artiméticos");
+ console.log(`3 + 4 = ${3 + 4}`);//ADIÇÃO
+ console.log(`3 - 4 = ${3 - 4}`);//SUBTRAÇÃO
+ console.log(`3 * 4 = ${3 * 4}`);//MULTIPLICAÇÃO
+ console.log(`19 / 3 = ${19 / 3}`);//DIVISÃO
+ console.log(`19 % 3 = ${19 % 5}`);//MODE | RESTO DA DIVISÃO
+ console.log(`3 ** 4 = ${3 ** 4}`);//ExPONECIAÇÃO 
+
+ //Operadores Comparativos
+ console.log("Operadores Comparativos");
+ 
