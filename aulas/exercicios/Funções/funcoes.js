98a268dd8fd18848fe2ad9be195d999be7bfe27b
Jeffdiginvice <jeffersondiginvice7@gmail.com>
2023-02-11 13:38:14 -0300

    atualizar

--- aulas/exercicios/Funções/funcoes.js
+++ aulas/exercicios/Funções/funcoes.js
@@ -35,3 +35,33 @@ function mostrarpreco(preco){
     console.log("preço = " + preco);
 }
 console.log(mostrarpreco(200))
+
+// Variavei definidas dentro do escopo da função não vazam escopo
+
+function areaCirculo(raio) {
+    const pi = 3.14;
+    return pi * raio * raio;
+}
+console.log(areaCirculo(3))
+
+//Function hoisting: Funções no JS são movidas para cima pelo motor do JavaScript 
+
+function teste(x) {
+    console.log("TESTE = " + x)
+}
+
+teste(10)
+
+// Funções podem ser passadas como argumento 
+
+function triplo(num) {
+    return num * 3;
+}
+
+function aplicar(f, num){
+    const result = f(num);
+    console.log("RESULTADO = " + result)
+}
+
+aplicar(triplo, 10)
+aplicar(dobro1, 10)
