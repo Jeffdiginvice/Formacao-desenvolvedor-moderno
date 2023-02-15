5ee2a5b0c59455586a1eb5d676ca458933769a81
Jeffdiginvice <jeffersondiginvice7@gmail.com>
2023-02-14 21:48:52 -0300

    atualizar

--- aulas/exercicios/funcao-construtora/func-construtora.js
+++ aulas/exercicios/funcao-construtora/func-construtora.js
@@ -1,36 +1,52 @@
 //Funções construtoras instanciam objetos com uma estrutura pré-definida
-
+//Boa prática: defina propriedades dentro do construtor, e métodos no prototype 
 const p1 = {
     name: "Computador",
     price: 3000.0,
     quantity: 2
 }
 console.log(p1)
-
+//função construtora
 const Product = function(name, price, quantity){
     this.name = name;
     this.price = price;
     this.quantity =  quantity;
-}
-
-Product.prototype.total = function() {
-    return this.price * this. quantity;
-}
-
-Product.prototype.add = function(amount){
-    if(this.quantity >= amount){
-        this.quantity = this.quantity + amount;
+    //se pode colocar uma função dentro do construtor | mesmo que se repita o nome da função, a prioridade é da função interna do objeto produto/product
+    Product.prototype.label = function(){
+        return `Dados: (Função interna) ${this.name}, ${this.price.toFixed(2)}`
     }
 }
 
-Product.prototype.remove = function(amount){
-    this.quantity -= remove;
+//Métodos
+Product.prototype.total = function() {
+    return this.price * this.quantity;
 }
+
+Product.prototype.add = function(amount){
+        this.quantity = this.quantity + amount; 
+}
+
+Product.prototype.remove = function(amount){
+    if (this.quantity >= amount){
+        this.quantity -= amount;
+    }
+}
+
+Product.prototype.label = function(){
+    return `Dados: ${this.name}, ${this.price.toFixed(2)}`
+}
+//-----------------
 
 const p2 = new Product("Monitor", 800.0, 10);
 
 console.log(`Valor: ${p2.total()}`);
+console.log(p2.label());
+console.log(p2.add(3));
+console.log(p2.remove(5));
 console.log(p2);
+console.log(p2.label());
 
 const p3 = new Product("Mouse", 50.0, 4);
+
 console.log(`Valor: ${p3.total()}`);
+
