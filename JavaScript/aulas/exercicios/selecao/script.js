a8d8c9ab966a110c55de943d3658391208199287
Jefferson Castro <jeffersondiginvice7@gmail.com>
2023-03-29 22:32:19 -0300

    atualização elementos

--- JavaScript/aulas/exercicios/selecao/script.js
+++ JavaScript/aulas/exercicios/selecao/script.js
@@ -6,16 +6,16 @@ console.log(item1.outerHTML);//Para escrever todo o HTML dele
 
 const items = document.querySelectorAll("li");
 
-console.log(items);
+console.log(items);  
     
 const card1 = document.querySelector(".card");
 console.log(card1);
 
-const paragrafo = document.createElement("p");
-paragrafo.innerHTML = "Descrição";
+const paragrafo = document.createElement("p");//estou criando o elemento P do HTML
+paragrafo.innerHTML = "Descrição";//coloquei o conteúdo descrição dentro do elemento P criado
 card1.appendChild(paragrafo);
 
-card1.classList.add("super-border")
+card1.classList.add("super-border")//classList devolve a lista de classes
 
 const cards = document.querySelectorAll(".card");
 const cardsName = document.getElementsByClassName("card");
